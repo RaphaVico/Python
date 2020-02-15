54734164d7a572d4b524f65c21054dbfce5f7c30
RaphaVico <35850763+RaphaVico@users.noreply.github.com>
2020-02-14 22:07:12 -0300

    Update regressão_linear.py

--- regressão_linear.py
+++ regressão_linear.py
@@ -7,7 +7,13 @@ from sklearn import metrics
 
 x = np.array([100,150,200,250,300,350,400,450]).reshape(-1,1)
 y = np.array([27.77777778,41.66666667,55.55555556,69.44444444,83.33333333,97.22222222,111.1111111,125]).reshape(-1,1)
-
+'''
+x = np.array(range(0,100)).reshape(-1,1)
+y = np.empty(len(x))
+for i in range(len(x)):
+    y[i] = randint(0,100)+i*2
+y = y.reshape(-1,1)
+'''
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)  #separando 20% para treinar
 regressor = LinearRegression()
@@ -20,7 +26,7 @@ b = regressor.intercept_
 print("y = ",a,"*x", "+",b) #equação da reta otima
 y1 = []
 for i in x:
-    y1.append((a*i)+b)
+    y1.append((a[0]*i)+b[0])
 print('Raiz quadrada do erro-médio',np.sqrt(metrics.mean_squared_error(y_test, y_pred)*100), "%") #porcentagem do erro testado em relção ao previsto
 plt.plot(x,y1, color = 'green')
 plt.show()
