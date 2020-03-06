279f89949b2a5a585ea31525e835b417e73310f3
RaphaVico <35850763+RaphaVico@users.noreply.github.com>
2020-03-06 11:40:25 -0300

    Update regressão_linear.py

--- regressão_linear.py
+++ regressão_linear.py
@@ -3,8 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 import numpy as np
-from sklearn import metrics
-
+import random 
 x = np.array([100,150,200,250,300,350,400,450]).reshape(-1,1)
 y = np.array([27.77777778,41.66666667,55.55555556,69.44444444,83.33333333,97.22222222,111.1111111,125]).reshape(-1,1)
 '''
